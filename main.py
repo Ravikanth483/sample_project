7d36e78bad73a4d865206f0fd57cac085f0c1194
mahesh jamdade <maheshmn121@gmail.com>
2025-01-27 12:43:12 -0500

    add a program for odd numbers

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+# program to find odd numbers in a list
+
+def odd_numbers(lst):
+    odd_lst = []
+    for i in lst:
+        if i % 2 != 0:
+            odd_lst.append(i)
+    return odd_lst
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(odd_numbers(lst))
---
+++ readme.md
@@ -0,0 +1,10 @@
+Readme for Git
+
+
+- Stage -> Commit -> Push
+
+Stage: git add <file>
+
+Commit: git commit -m "message"
+
+Push: git push origin master
